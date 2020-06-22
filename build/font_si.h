a177ddc61becd3b4d300b87134a44e7704557356
jackokring <jackokring@gmail.com>
2020-06-23 00:23:44 +0100

    ok

--- build/font_si.h
+++ build/font_si.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 512 + 65536 = 66048
 //
-//	Time-stamp: 2020-06-22, 21:58:59
+//	Time-stamp: 2020-06-23, 00:22:37
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
