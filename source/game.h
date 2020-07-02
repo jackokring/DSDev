9bff75a5603f70f22994b0800c49345cd8bf20cd
jackokring <jackokring@gmail.com>
2020-07-02 21:51:09 +0100

    headers

--- source/game.h
+++ source/game.h
@@ -1,6 +1,8 @@
 #ifndef GAME__H
 #define GAME__H
 #include "ctl.h"
+#include "gfx.h"
+#include "sfx.h"
 #include <nds.h>
 #include <gl2d.h>
 //============================== GAME INCLUDES ====================
